8bedfc6da5d041b0eb20922d282656bca68c097f
AltovoIta <cavila@fi.uba.ar>
2025-08-05 20:02:55 -0300

    refactor - 4.2: created blog controller

--- part4/blog-backend/index.js
+++ part4/blog-backend/index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const Blog = require('./models/blog')
-const app = express()
+const blogRouter = require('./controllers/blog')
 
+const app = express()
 
 logger.info('Connecting to MongoDB...')
 
@@ -15,19 +15,7 @@ mongoose.connect(mongoUrl)
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
