1e2ee9821132d492a88382c98bf9dacd329470cf
AltovoIta <cavila@fi.uba.ar>
2025-08-05 21:13:35 -0300

    feat: exercise 4.5 DONE

--- part4/blog-backend/utils/list_helper.js
+++ part4/blog-backend/utils/list_helper.js
@@ -8,7 +8,16 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favouriteBlog = (blogs) => {
+    if (blogs.length === 0) return null
+
+    return blogs.reduce((favourite, blog) => {
+        return (favourite.likes || 0) < blog.likes ? blog : favourite
+    }, {})
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
 }
