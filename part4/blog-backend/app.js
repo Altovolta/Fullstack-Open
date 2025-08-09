46f23766f749eac459b26846c1327117adc296d0
AltovoIta <cavila@fi.uba.ar>
2025-08-08 22:00:25 -0300

    exercise 4.18 - jwt auth  - DONE

--- part4/blog-backend/app.js
+++ part4/blog-backend/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const blogRouter = require('./controllers/blog')
 const userRouter = require('./controllers/user')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 const app = express()
@@ -21,6 +22,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
