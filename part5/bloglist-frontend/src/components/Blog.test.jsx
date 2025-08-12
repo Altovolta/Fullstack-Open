3fb1e23c59ca48a71db598be04143a786e9bd2ef
AltovoIta <cavila@fi.uba.ar>
2025-08-12 20:16:26 -0300

    exercise 5.13 - refactor in 2 tests

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,22 +1,23 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 
 describe('<Blog />', () => {
-
-  test('Only displays author and title by default', async () => {
-    const blog = {
-      id: 'asdeasd45784d5',
-      title: 'Ficciones',
-      author: 'Borges',
-      url: 'http://ficcion-botges.com',
-      likes: 15,
-      user: {
-        id: 'asdw44544asdw',
-        name: 'Pedro',
-        username: 'Pepe'
-      }
+  const blog = {
+    id: 'asdeasd45784d5',
+    title: 'Ficciones',
+    author: 'Borges',
+    url: 'http://ficcion-botges.com',
+    likes: 15,
+    user: {
+      id: 'asdw44544asdw',
+      name: 'Pedro',
+      username: 'Pepe'
     }
+  }
+
+  test('displays author and title by default', async () => {
 
     const onLike = vi.fn()
     const removeBlog = vi.fn()
@@ -27,6 +28,14 @@ describe('<Blog />', () => {
 
     expect(div).toHaveTextContent('Ficciones')
     expect(div).toHaveTextContent('Borges')
+  })
+
+  test('at start does not display blog details', async () => {
+
+    const onLike = vi.fn()
+    const removeBlog = vi.fn()
+
+    const { container } = render(<Blog blog={blog} onLike={onLike} removeBlog={removeBlog} />)
 
     const blogDetails = container.querySelector('.blogDetails')
     expect(blogDetails).toHaveStyle('display: none')
