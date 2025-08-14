c064fa143eff9d0657cff5426c070fed997af04c
AltovoIta <cavila@fi.uba.ar>
2025-08-14 16:55:48 -0300

    exercise 6.3 - DONE

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -22,8 +22,28 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
+  switch (action.type) {
+    case 'VOTE': {
+      const id = action.payload.id
+      const oldNote = state.find(note => note.id === id)
 
-  return state
+      const updatedNote = {
+        ...oldNote, 
+        votes: oldNote.votes + 1
+      }
+      return state.map(note => note.id !== id ? note : updatedNote)
+    }
+    default: return state
+      
+  }
+
+}
+
+export const voteNote = (id) => {
+  return {
+    type: 'VOTE',
+    payload: { id }
+  }
 }
 
 export default reducer
