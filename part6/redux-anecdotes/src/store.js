0d14fc0500787cca221c0b15438ae838c8c6a3a2
AltovoIta <cavila@fi.uba.ar>
2025-08-16 17:06:59 -0300

    refactor: moved store to own file

---
+++ part6/redux-anecdotes/src/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import anecdoteReducer from './reducers/anecdoteReducer'
+import filterReducer from './reducers/filterReducer'
+import notifiacationReducer from './reducers/notificationReducer'
+
+
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filter: filterReducer,
+    notification: notifiacationReducer
+  }
+})
+
+export default store
