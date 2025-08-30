5067341e0b90e3e829357668b134b53a543955f8
AltovoIta <cavila@fi.uba.ar>
2025-08-30 16:18:18 -0300

    exercise 8.7 - DONE

--- part8/backend-graphql/index.js
+++ part8/backend-graphql/index.js
@@ -110,6 +110,7 @@ const typeDefs = `
       published: Int, 
       genres: [String!]
     ): Book, 
+    editAuthor(name: String!, setBornTo: Int): Author
   }
 
   type Author {
@@ -153,6 +154,18 @@ const resolvers = {
         authors = authors.concat(author)
       }
       return book
+    },
+    editAuthor: (root, args) => {
+      const author =  authors.find(author => author.name === args.name)
+      if(!author) { return null }
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(author => author.name === args.name 
+        ? updatedAuthor
+        : author
+      )
+
+      return updatedAuthor
     }
   }
 }
