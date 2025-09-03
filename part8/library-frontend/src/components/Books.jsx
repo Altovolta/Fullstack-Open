e98c3ad1a409d6e9a3107717616cd1b86b0d8afb
AltovoIta <cavila@fi.uba.ar>
2025-09-03 16:55:56 -0300

    exercise 8.19: DONE

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react"
 import { ALL_BOOKS } from "../queries"
 import { useQuery } from "@apollo/client/react"
 
 const Books = (props) => {
+
+  const [filter, setFilter] = useState('all genres')
 
   const result = useQuery(ALL_BOOKS)
   if (!props.show) {
@@ -12,12 +15,39 @@ const Books = (props) => {
     return (<div> Is loading...</div>)
   }
 
+
   const books = result.data.allBooks
+
+  const getGenres = () => {
+    const genres = new Set()
+
+      books.forEach(book => {
+        genres.add(...book.genres)
+      })
+
+      genres.add('all genres')
+      return Array.from(genres)
+    }
+
+  const uniqueGenres = getGenres()
+
+  const filteredBooks = books.filter(book => 
+    filter === 'all genres' || book.genres.includes(filter)
+  )
 
   return (
     <div>
       <h2>books</h2>
-
+      <h4>
+        In genre: {filter}
+      </h4>
+      {uniqueGenres.map(genre => 
+      <button 
+        key={genre} 
+        onClick={() => setFilter(genre)}>
+        {genre}
+      </button>
+      )}
       <table>
         <tbody>
           <tr>
@@ -25,7 +55,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
