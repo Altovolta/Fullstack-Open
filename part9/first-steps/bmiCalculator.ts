afd10857364f58efd602ec42fcbbfda16f6ab056
AltovoIta <cavila@fi.uba.ar>
2025-09-05 18:19:28 -0300

    exercise 9.1 - DONE

---
+++ part9/first-steps/bmiCalculator.ts
@@ -0,0 +1,19 @@
+const calculateBmi = (height: number, weight:number): string => {
+
+    const bmi: number =  weight / ((height / 100) ** 2)
+
+    if (bmi < 18.5) {
+        return "Underweight";
+    } 
+    else if (bmi < 25) {
+        return "Normal range";
+    }
+    else if (bmi < 30 ) {
+        return "Overweight";
+    }
+    
+    return "Obese";
+
+}
+
+console.log(calculateBmi(180, 74))
