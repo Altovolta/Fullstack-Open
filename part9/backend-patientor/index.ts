0f07561f360eceb7e7cf6d764c035015bdbb55b8
AltovoIta <cavila@fi.uba.ar>
2025-09-08 17:11:56 -0300

    exercise 9.9 - added cors - DONE

--- part9/backend-patientor/index.ts
+++ part9/backend-patientor/index.ts
@@ -1,12 +1,14 @@
 import express, { Express } from 'express';
+import cors from 'cors';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
 const app: Express = express();
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
 app.use(express.json());
+app.use(cors);
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/ping', (_req, res) => {
   res.send('pong');
