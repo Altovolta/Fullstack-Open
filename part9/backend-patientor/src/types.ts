7d35418beaabaf7ec705e52571431d809810d626
AltovoIta <cavila@fi.uba.ar>
2025-09-12 18:36:44 -0300

    exercise 9.23 - DONE

--- part9/backend-patientor/src/types.ts
+++ part9/backend-patientor/src/types.ts
@@ -12,10 +12,53 @@ export enum Gender {
   Other = 'other'
 }
 
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
 // can use it as the course intends cuz im using zod
-export const Entry  = z.object({
+export const BaseEntry  = z.object({
+  id: z.string(),
+  description: z.string(),
+  date: z.iso.date(),
+  specialist: z.string(),
+  diagnosisCodes: z.string().array().optional()
 });
 
+export const OccupationalHealthcareEntry = BaseEntry.extend({
+  type: z.literal("OccupationalHealthcare"),
+  employerName: z.string(),
+  sickLeave: z.object({
+    startDate: z.iso.date(),
+    endDate: z.iso.date(),
+  }).optional()
+});
+
+
+export const HospitalEntry = BaseEntry.extend({
+  type: z.literal("Hospital"),
+  discharge: z.object({
+    date: z.iso.date(),
+    criteria: z.string()
+  })
+});
+
+
+export const HealthCheckEntry = BaseEntry.extend({
+  type: z.literal("HealthCheck"),
+  healthCheckRating: z.enum(HealthCheckRating)
+});
+
+export const EntrySchema = z.discriminatedUnion("type", [
+  HospitalEntry,
+  OccupationalHealthcareEntry,
+  HealthCheckEntry
+]);
+
+export type Entry = z.infer<typeof EntrySchema>;
 
 export const newPatientSchema = z.object({
   name: z.string(),
@@ -23,7 +66,7 @@ export const newPatientSchema = z.object({
   ssn: z.string(),
   gender: z.enum(Gender),
   occupation: z.string(),
-  entries: z.array(Entry),
+  entries: z.array(EntrySchema),
 });
 
 export type NewPatient = z.infer<typeof newPatientSchema>;
