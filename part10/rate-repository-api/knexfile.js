8c612b50d417bc675eb9b2e1b5655a657b07b802
AltovoIta <cavila@fi.uba.ar>
2025-09-19 15:42:48 -0300

    added backed for part10

---
+++ part10/rate-repository-api/knexfile.js
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+
+const FILENAME = process.env.DATABASE_FILENAME || 'database.sqlite';
+
+export default {
+  client: 'sqlite3',
+  connection: {
+    filename: FILENAME,
+  },
+  useNullAsDefault: true,
+};
