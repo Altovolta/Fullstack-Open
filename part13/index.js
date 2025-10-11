702518f43e72d343d4d80b7c49045c06dda5a9f6
AltovoIta <cavila@fi.uba.ar>
2025-10-11 17:53:05 -0300

    exercise 13.4 - POST /api/blogs DONE

--- part13/index.js
+++ part13/index.js
@@ -3,10 +3,21 @@ const express = require('express');
 const Blog = require('./models/Blog');
 
 const app = express()
+app.use(express.json())
+
 
 app.get('/api/blogs', async (req, res) => {
   const blogs = await Blog.findAll()
   res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    res.json(blog)
+  } catch (error) {
+    res.status(400).send({error})
+  }
 })
 
 
